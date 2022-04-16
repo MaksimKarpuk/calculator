e3adc2c3092cf1cfcb6cf438c360bc21a47d84ae
Maksim <maksim.karpuk13910@gmail.com>
2022-04-16 10:25:01 +0300

    feature/2

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,56 +1,49 @@
-let min = document.querySelector (".min");
+let min = document.querySelector(".min");
 let seconds = document.querySelector(".sec");
-let start = document.querySelector (".start");
-let stopTimer = document.querySelector (".stop");
-let reset = document.querySelector (".reset");
-let field = document.querySelector (".field");
-let timerInterval
+let start = document.querySelector(".start");
+let stopTimer = document.querySelector(".stop");
+let reset = document.querySelector(".reset");
+let field = document.querySelector(".field");
+let timerInterval;
 const timerReset = () => {
-    clearInterval(timerInterval);
-    field.innerHTML="00:00"
-    min.value=0;
-    seconds.value=0;
-    start.removeAttribute("disabled");
-}
+  clearInterval(timerInterval);
+  field.innerHTML = "00:00";
+  min.value = 0;
+  seconds.value = 0;
+  start.removeAttribute("disabled");
+};
 const timerWork = () => {
-    // if (+seconds.value<=10){
-    //     +seconds.value--;
-    //     field.innerHTML = `${+min.value}:0${+seconds.value}`; 
-    //     if (+seconds.value===0){timerReset()}}
-    //     else
-    if (+seconds.value!==0) {
-        +seconds.value--;
-        field.innerHTML = `${+min.value}:${+seconds.value}`;
-    } else
-    //  if (min.value<=10){
-    //     +seconds.value--;
-    //     field.innerHTML = `0${+min.value}:${+seconds.value}`; 
-    //     // if (+min.value===0){timerReset()}
-    // } else
-    if (+seconds.value===0 && +min.value!==0){
-        seconds.value = 59;
-        +min.value--;
-        field.innerHTML =`${+min.value}:${+seconds.value}`;
-    } else if (+seconds.value===0 && +min.value===0) {
-        timerReset()
-    } 
-}
-const timerStart = () => {
-    if (+seconds.value<0 || +min.value<0) {
-        alert("Неверное значение!")
-        return
+  if (+seconds.value !== 0) {
+    +seconds.value--;
+    field.innerHTML = `${+min.value}:${+seconds.value}`;
+    if (+seconds.value < 10) {
+      field.innerHTML = `${+min.value}:0${+seconds.value}`;
     }
-    timerInterval = setInterval(timerWork,1000);
-    start.setAttribute("disabled", "true");
-
-}
+  } else if (+min.value < 10 && +min.value >= 0 && +seconds.value === 0) {
+    seconds.value = 59;
+    +min.value--;
+    field.innerHTML = `0${+min.value}:${+seconds.value}`;
+  } else if (+seconds.value === 0 && +min.value !== 0) {
+    seconds.value = 59;
+    +min.value--;
+    field.innerHTML = `${+min.value}:${+seconds.value}`;
+  } else if (+seconds.value === 0 && +min.value === 0) {
+    timerReset();
+  }
+};
+const timerStart = () => {
+  if (+seconds.value < 0 || +min.value < 0) {
+    alert("Неверное значение!");
+    return;
+  }
+  timerInterval = setInterval(timerWork, 1000);
+  start.setAttribute("disabled", "true");
+};
 const timerStop = () => {
-    clearInterval(timerInterval);
-    start.removeAttribute("disabled");
-}
+  clearInterval(timerInterval);
+  start.removeAttribute("disabled");
+};
 
 start.addEventListener("click", timerStart);
 stopTimer.addEventListener("click", timerStop);
 reset.addEventListener("click", timerReset);
-
-
